bd8272f4f64f706e0e2dda6cc1133013f105b19a
iamdLite <70290973+IamdLite@users.noreply.github.com>
2023-03-04 15:04:43 +0100

    Create make.py

---
+++ make.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+# Specify the program name and source file
+program_name = "My Excel Filter"
+source_file = "pd.py"
+
+# Create the executable
+executable = Executable(script=source_file, targetName=program_name)
+
+# Define options for the setup
+options = {
+    'build_exe': {
+        'packages': ['pandas', 'unicodedata', 'tkinter', 'openpyxl'],
+        'include_files': []
+    }
+}
+
+# Setup the application
+setup(name=program_name, version='1.0', description='My program', options=options, executables=[executable])
